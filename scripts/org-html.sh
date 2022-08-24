92bf3fc9c2624ed8f6972c4ccdaa87bbadfb1e49
Masataro Asai <guicho2.71828@gmail.com>
2022-08-24 10:52:07 -0400

    [cherry-pick] EMACS can be inherited from the environment

--- scripts/org-html.sh
+++ scripts/org-html.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+EMACS=${EMACS:-emacs}
+
 in=$(readlink -ef $1)
 out=$(readlink -ef $2)
 cd $(dirname $(readlink -ef $0))
-emacs --batch --quick --eval "(progn (load-file \"compile-org-html.el\")(compile-org \"$in\" \"$out\"))"
+$EMACS --batch --quick --eval "(progn (load-file \"compile-org-html.el\")(compile-org \"$in\" \"$out\"))"
 
