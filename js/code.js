ecb8cb00de05193f3023c449ca0391bebc3cb62e
Masataro Asai <guicho2.71828@gmail.com>
2022-11-25 13:08:56 -0500

    removed unused function

--- js/code.js
+++ js/code.js
@@ -13,15 +13,6 @@
 
 var identity=function(){};
 
-jQuery.fn.visible = function() {
-    return this.css('visibility', 'visible');
-};
-
-jQuery.fn.invisible = function() {
-    return this.css('visibility', 'hidden');
-};
-
-
 jQuery.fn.emerge = function() {
     return this.addClass("emerge");
 };
